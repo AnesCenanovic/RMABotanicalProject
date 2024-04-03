37890a998165d36ebd17b49040b4058991608ede
AnesCenanovic <127337745+AnesCenanovic@users.noreply.github.com>
2024-04-03 13:11:18 +0200

    Dodavanje novih biljaka

--- app/src/main/java/com/example/lab2_cinaeste/MainActivity.kt
+++ app/src/main/java/com/example/lab2_cinaeste/MainActivity.kt
@@ -174,10 +174,7 @@ class MainActivity : AppCompatActivity() {
             naziv = "Bosiljak (Ocimum basilicum)",
             porodica = "Lamiaceae (usnate)",
             medicinskoUpozorenje = "Može iritati kožu osjetljivu na sunce. Preporučuje se oprezna upotreba pri korištenju ulja bosiljka.",
-            medicinskeKoristi = listOf(
-                MedicinskaKorist.SMIRENJE,
-                MedicinskaKorist.REGULACIJAPROBAVE
-            ),
+            medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE, MedicinskaKorist.REGULACIJAPROBAVE),
             profilOkusa = ProfilOkusaBiljke.BEZUKUSNO,
             jela = listOf("Salata od paradajza", "Punjene tikvice"),
             klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUBTROPSKA),
@@ -207,12 +204,9 @@ class MainActivity : AppCompatActivity() {
             naziv = "Ružmarin (Rosmarinus officinalis)",
             porodica = "Lamiaceae (metvice)",
             medicinskoUpozorenje = "Treba ga koristiti umjereno i konsultovati se sa ljekarom pri dugotrajnoj upotrebi ili upotrebi u većim količinama.",
-            medicinskeKoristi = listOf(
-                MedicinskaKorist.PROTUUPALNO,
-                MedicinskaKorist.REGULACIJAPRITISKA
-            ),
+            medicinskeKoristi = listOf(MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.REGULACIJAPRITISKA),
             profilOkusa = ProfilOkusaBiljke.AROMATICNO,
-            jela = listOf("Pečeno pile", "Grah", "Gulaš"),
+            jela = listOf("Pečeno pile", "Grah","Gulaš"),
             klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUHA),
             zemljisniTipovi = listOf(Zemljiste.SLJUNOVITO, Zemljiste.KRECNJACKO)
         ),
@@ -225,6 +219,56 @@ class MainActivity : AppCompatActivity() {
             jela = listOf("Jogurt sa voćem"),
             klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUHA),
             zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.KRECNJACKO)
+        ),
+        Biljka(
+            naziv = "Peršun (Petroselinum crispum)",
+            porodica = "Apiaceae (štitarka)",
+            medicinskoUpozorenje = "Osobe koje pate od alergija na korijandar, mrkvu ili mašću također mogu biti alergične na peršin.",
+            medicinskeKoristi = listOf(MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.REGULACIJAPROBAVE),
+            profilOkusa = ProfilOkusaBiljke.LJUTO,
+            jela = listOf("Juha od peršuna", "Salata od peršina"),
+            klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUBTROPSKA),
+            zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.ILOVACA)
+        ),
+        Biljka(
+            naziv = "Kopriva (Urtica dioica)",
+            porodica = "Urticaceae (koprivke)",
+            medicinskoUpozorenje = "Izbjegavajte dodir s koprivom ako ste alergični na nju.",
+            medicinskeKoristi = listOf(MedicinskaKorist.IMMUNOSUPORT, MedicinskaKorist.SMIRENJE),
+            profilOkusa = ProfilOkusaBiljke.LJUTO,
+            jela = listOf("Čaj od koprive", "Pesto od koprive"),
+            klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUHA),
+            zemljisniTipovi = listOf(Zemljiste.SLJUNOVITO, Zemljiste.KRECNJACKO)
+        ),
+        Biljka(
+            naziv = "Matičnjak (Melissa officinalis)",
+            porodica = "Lamiaceae (metvice)",
+            medicinskoUpozorenje = "Oprez kod osoba s hipotireozom, budući da može sniziti razinu hormona.",
+            medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE, MedicinskaKorist.IMMUNOSUPORT),
+            profilOkusa = ProfilOkusaBiljke.CITRUSNI,
+            jela = listOf("Čaj od matičnjaka", "Koktel s matičnjakom"),
+            klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SREDOZEMNA),
+            zemljisniTipovi = listOf(Zemljiste.SLJUNOVITO, Zemljiste.PJESKOVITO)
+        ),
+        Biljka(
+            naziv = "Matricaria chamomilla",
+            porodica = "Asteraceae (glavočike)",
+            medicinskoUpozorenje = "Može uzrokovati alergijske reakcije kod osjetljivih osoba.",
+            medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE, MedicinskaKorist.PROTUUPALNO),
+            profilOkusa = ProfilOkusaBiljke.AROMATICNO,
+            jela = listOf("Čaj od kamilice"),
+            klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUBTROPSKA),
+            zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.KRECNJACKO)
+        ),
+        Biljka(
+            naziv = "Hibiscus (Hibiscus sabdariffa)",
+            porodica = "Malvaceae (sljezovke)",
+            medicinskoUpozorenje = "Ne preporučuje se za trudnice i dojilje.",
+            medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE, MedicinskaKorist.PROTUUPALNO),
+            profilOkusa = ProfilOkusaBiljke.CITRUSNI,
+            jela = listOf("Čaj od hibiskusa", "Hibiskusni sok"),
+            klimatskiTipovi = listOf(KlimatskiTip.TROPSKA, KlimatskiTip.SUBTROPSKA),
+            zemljisniTipovi = listOf(Zemljiste.ILOVACA, Zemljiste.SLJUNOVITO)
         )
     )
 
